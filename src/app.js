1da544b4a6f739187fb487e389d064d9190a1313
Aadi-patel04 <aadipatel0042@gmail.com>
2025-07-13 01:11:43 +0530

    util code discussion

--- src/app.js
+++ src/app.js
@@ -1,5 +1,24 @@
-// import express from "express"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-// const app=express();
+const app = express();
 
-// export {app}
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN, // konsa domain can acess your server
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" })); // form bharne pai agar data le too
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(express.static("pulic"));
+
+app.use(cookieParser());
+
+// app.use(cors())  to configure it
+
+export { app };
